8eb202058b5e057f349955005b2581d8935189c8
Eli Levy Karin <eli.levy.karin@gmail.com>
2020-06-19 15:36:20 +0200

    fixed path to results

--- test_agg_tax.sh
+++ test_agg_tax.sh
@@ -6,8 +6,8 @@ RESULTPATH="$3"
 
 mkdir -p "${RESULTPATH}"
 
-"${METAEUK}" easy-predict "${DATAPATH}/NC_001133.9_100425_153163.fa" "${DATAPATH}/sacc_prots_in_segment.fa" "${RESULTPATH}/preds" tempFolder
-"${METAEUK}" taxtocontig "tempFolder/latest/contigs" "preds.fas" "preds.headersMap.tsv" "${DATAPATH}/swissProtSomeClasses" "taxRes" "tmpDir" -s 2 --majority 0.5 --tax-lineage --lca-mode 2
+"${METAEUK}" easy-predict "${DATAPATH}/NC_001133.9_100425_153163.fa" "${DATAPATH}/sacc_prots_in_segment.fa" "${RESULTPATH}/preds" "${RESULTPATH}/tempFolder"
+"${METAEUK}" taxtocontig "${RESULTPATH}/tempFolder/latest/contigs" "${RESULTPATH}/preds.fas" "${RESULTPATH}/preds.headersMap.tsv" "${DATAPATH}/swissProtSomeClasses" "${RESULTPATH}/taxRes" "${RESULTPATH}/tmpDir" -s 2 --majority 0.5 --tax-lineage --lca-mode 2
 
 # check output #
-(grep "Saccharomyces" taxRes_tax_per_contig.tsv) || (echo "did not match Saccharomyces"; exit 1)
+(grep "Saccharomyces" "${RESULTPATH}/taxRes_tax_per_contig.tsv") || (echo "did not match Saccharomyces"; exit 1)
