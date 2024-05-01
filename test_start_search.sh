c9834d5cdc977b944cf0f02679fb4d2acb28d860
Eli Levy Karin <eli.levy.karin@gmail.com>
2024-05-01 17:55:47 +0200

    param name changed

--- test_start_search.sh
+++ test_start_search.sh
@@ -6,8 +6,8 @@ RESULTPATH="$3"
 
 mkdir -p "${RESULTPATH}"
 
-"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/resNoSearch" tempFolder --len-search-start 0
-"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/resWithSearch" tempFolder --len-search-start 140
+"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/resNoSearch" tempFolder --len-scan-for-start 0
+"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/resWithSearch" tempFolder --len-scan-for-start 140
 
 # check output #
 perl compare_fasta_results.pl "${RESULTPATH}/resWithSearch.fas" "${RESULTPATH}/resNoSearch.fas" "${DATAPATH}/as_should_with_search.fas" "${DATAPATH}/as_should_no_search.fas"
