36ed1373bd7ee55981ddf4bbd104884720491daa
Eli Levy Karin <eli.levy.karin@gmail.com>
2024-05-01 15:53:41 +0200

    added start search test

--- run.sh
+++ run.sh
@@ -15,3 +15,5 @@
 
 ./test_agg_tax.sh "$1" sacc_tax sacc_tax_results
 
+./test_start_search "$1" start_search start_search_results
+
---
+++ test_start_search.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+METAEUK="$1"
+DATAPATH="$2"
+RESULTPATH="$3"
+
+mkdir -p "${RESULTPATH}"
+
+"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/resNoSearch" tempFolder --len-search-start 0
+"${METAEUK}" easy-predict "${DATAPATH}/contigs.fna" "${DATAPATH}/proteins.faa" "${RESULTPATH}/resWithSearch" tempFolder --len-search-start 140
+
+# check output #
+perl compare_fasta_results.pl "${RESULTPATH}/resWithSearch.fas" "${RESULTPATH}/resNoSearch.fas" "${DATAPATH}/as_should_with_search.fas" "${DATAPATH}/as_should_no_search.fas"
